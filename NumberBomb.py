705bc7fce032fb943d5e9556cde7c95ed475e13c
Benson_Hsieh <benson711711@gmail.com>
2019-04-07 01:00:48 +0800

    fix bug

--- NumberBomb.py
+++ NumberBomb.py
@@ -6,23 +6,22 @@ maxNum = 100
 bombNumber = randint(minNum, maxNum)
 
 while isGameOver == False:
-    guessNumber = input(f"輸入{minNum}~{maxNum}內任一數字:")
+    if minNum == maxNum:
+        print(f"沒得猜了！爆爆數字為{minNum}！")
+    else:
+        guessNumber = input(f"輸入{minNum}~{maxNum}內任一數字:")
 
-    try:
-        guessNumber = int(guessNumber)
+        try:
+            guessNumber = int(guessNumber)
 
-        if guessNumber > maxNum or guessNumber < minNum:
-            print("輸入數字範圍錯誤！")
-        elif guessNumber == bombNumber:
-            print(f"BOOM！爆爆數字為{bombNumber}！")
-            isGameOver = True
-        elif guessNumber == minNum and guessNumber != minNum + 1:
-            minNum = minNum + 1
-        elif guessNumber == maxNum and guessNumber != maxNum - 1:
-            maxNum = maxNum - 1                
-        elif guessNumber > minNum and guessNumber < bombNumber:
-            minNum = guessNumber
-        else:
-            maxNum = guessNumber
-    except:
-        print("請輸入數字！")
+            if guessNumber > maxNum or guessNumber < minNum:
+                print("輸入數字範圍錯誤！")
+            elif guessNumber == bombNumber:
+                print(f"BOOM！爆爆數字為{bombNumber}！")
+                isGameOver = True         
+            elif guessNumber >= minNum and guessNumber < bombNumber:
+                minNum = guessNumber + 1
+            else:
+                maxNum = guessNumber - 1
+        except:
+            print("請輸入數字！")
